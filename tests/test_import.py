4c7082f1121db011e21df1e0edc88936729d479f
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-07-21 16:08:56 -0600

    ci: fix test

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,3 +1,3 @@
 def test_import():
-    from peacock_trame import main  # noqa: F401
+    from peacock_trame.app import main  # noqa: F401
     from peacock_trame.widgets.peacock import Editor  # noqa: F401
